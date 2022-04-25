bfe63ac10e68814d5bf33679466bfd4ed62b656e
Дневник UNIX'оида <84270488+thm-unix@users.noreply.github.com>
2022-04-25 12:59:14 +0300

    Update about.cpp

--- about.cpp
+++ about.cpp
@@ -29,7 +29,7 @@ about::about(QWidget *parent)
     ui->setupUi(this);
 
     this->setWindowTitle("About plainDE");
-    ui->versionLabel->setText("0.0.3");
+    ui->versionLabel->setText("0.0.3a");
 
     readConfig();
 
